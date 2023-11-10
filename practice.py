56b743fe03d5835c37754ebd262fbce68146374d
cindiwidia <cindiwidia913@gmail.com>
2023-11-10 19:32:13 +0700

    Update

---
+++ practice.py
@@ -0,0 +1,12 @@
+import requests
+
+api_key = '74683838-c0ab-471a-a808-0f72c892dc34'
+word = 'potato'
+url = f'https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={api_key}'
+
+res = requests.get(url)
+
+definitions = res.json()
+
+for definition in definitions:
+    print(definition)
